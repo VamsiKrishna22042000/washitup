f6e4e3c911df06f6ac1cb8fb13367aa483bf7de3
VamsiKrishna22042000 <vamsi45678901@gmail.com>
2023-09-01 12:49:30 +0530

    User commit 2

--- laundryman/src/components/LaundryBody/index.js
+++ laundryman/src/components/LaundryBody/index.js
@@ -139,7 +139,12 @@ class LaundryBody extends Component {
       <div className="background-2">
         <h1 className="back2-head">
           Why To Choose
-          <span className="span2">{"                   "} WashIt Up </span>?
+          <span className="span2"> WashIt Up </span>?
+        </h1>
+        <h1 className="back2-head2">
+          Why To Choose
+          <br />
+          <span className="span2"> WashIt Up </span>?
         </h1>
         <p className="tagline"></p>
 
