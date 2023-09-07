71fce3e7ded97f3785cca2fec77342293c3459a8
VamsiKrishna22042000 <vamsi45678901@gmail.com>
2023-09-07 11:08:54 +0530

    made desing changes for the items 2

--- laundryman/src/components/ProtectedRoute/protectedroute.js
+++ laundryman/src/components/ProtectedRoute/protectedroute.js
@@ -1,15 +1,7 @@
 import { Route } from "react-router-dom";
 
-import Cookies from "js-cookie";
-
 const ProtectedRoute = (props) => {
-  let obtainedData = Cookies.get("jwt_token");
-
-  if (obtainedData === undefined) {
-    return <Route {...props} />;
-  } else if (obtainedData !== undefined) {
-    return <Route {...props} />;
-  }
+  <Route {...props} />;
 };
 
 export default ProtectedRoute;
