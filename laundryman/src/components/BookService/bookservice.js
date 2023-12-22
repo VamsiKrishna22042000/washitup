fe3760da323dd461e4a8a0efeb830d536a039038
VamsiKrishna22042000 <vamsi45678901@gmail.com>
2023-12-22 13:44:09 +0530

    made changes to the date

--- laundryman/src/components/BookService/bookservice.js
+++ laundryman/src/components/BookService/bookservice.js
@@ -66,10 +66,10 @@ const BookService = (props) => {
   );
 
   const min = new Date();
-  min.setDate(min.getDate() - 30);
+  min.setDate(min.getDate() + 1);
 
   const max = new Date();
-  max.setDate(max.getDate() + 30);
+  max.setDate(max.getDate() + 7);
 
   const [date, setDate] = useState(min);
 
